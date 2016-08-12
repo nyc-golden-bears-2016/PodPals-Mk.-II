dec2677021a2da0ef5d9ab285bde5531c067ee0a
Joe Wood <email": null>
2016-08-12 16:24:17 -0400

    installs rspec and provides user email tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe User, "#email" do
+    it 'has an email' do
+      test_user = User.create(email: "test@gmail.com")
+      expect(test_user.email).to eq("test@gmail.com")
+    end
+
+    it 'has a unique email address' do
+      test1 = User.create(email: "test@gmail.com")
+      test2 = User.create(email: "test@gmail.com")
+      expect(test2.id).to be(nil)
+    end
+  end
 end
