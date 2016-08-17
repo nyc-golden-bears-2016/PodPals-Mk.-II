a8227d4b5de9dc11ff0baf661e32c980de869d81
Christopher Danella <christopher.danella@gmail.com>
2016-08-17 11:23:23 -0400

    Update subject message for mailer

--- app/mailers/podcast_mailer.rb
+++ app/mailers/podcast_mailer.rb
@@ -4,6 +4,6 @@ class PodcastMailer < ApplicationMailer
   def share_podcast(user)
     @user = user
     @url = url
-    mail(to: @user.email, subject: 'New Podcast Shared with you!')
+    mail(to: @user.email, subject: 'PodPals: New Podcast Shared with you!')
   end
 end
