a9c0d402d90e0c64ffb548908f5804198aa28be3
Aleksander Turberville <aleksville@live.com>
2016-08-17 10:13:40 -0400

    working out bugs for user profile pictures

---
+++ db/migrate/20160817141207_add_attachment_avatar_to_users.rb
@@ -0,0 +1,12 @@
+class AddAttachmentAvatarToUsers < ActiveRecord::Migration[5.0]
+  def change
+  	def self.up
+    	change_table :users do |t|
+      		t.attachment :avatar
+    	end
+  	end
+
+  	def self.down
+    	remove_attachment :users, :avatar
+  	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816182447) do
+ActiveRecord::Schema.define(version: 20160816155928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 20160816182447) do
   end
 
   create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",       null: false
     t.integer  "likeable_id"
     t.string   "likeable_type"
-    t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
