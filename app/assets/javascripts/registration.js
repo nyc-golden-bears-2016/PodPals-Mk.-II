3ce501a35f2664cdb64c0d4a7a53c0a4b25e2c59
Joe Wood <email": null>
2016-08-19 09:59:58 -0400

    removed file upload code

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  document.getElementById("uploadBtn").onchange = function () {
-      document.getElementById("uploadFile").value = this.value;
-  };
-});
+// $(document).ready(function() {
+//   document.getElementById("uploadBtn").onchange = function () {
+//       document.getElementById("uploadFile").value = this.value;
+//   };
+// });
